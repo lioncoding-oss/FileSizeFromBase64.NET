26d7db677c14674c167f8d75f39fba55b913f1af
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2022-02-14 19:10:33 +0100

    tests: added features tests

---
+++ tests/FileSizeFromBase64.NET.Tests/FileSizeHelpersTests.cs
@@ -0,0 +1,24 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace FileSizeFromBase64.NET.Tests
+{
+    public class FileSizeHelpersTests
+    {
+
+        [Fact]
+        public void  GetFileSizeFromBase64String_Should_Return_0_If_String_Is_Null()
+        {
+            var size = FileSizeHelpers.GetFileSizeFromBase64String(null);
+            size.ShouldBe(0);
+        }
+
+        [Theory]
+        [ClassData(typeof(Base64FileTheoryData))]
+        public void GetFileSizeFromBase64String_Should_Return_The_Right_Size(Base64File base64File)
+        {
+            var size = FileSizeHelpers.GetFileSizeFromBase64String(base64File.Base64String, base64File.UsePaddingsRules);
+            size.ShouldBe(base64File.Size);
+        }
+    }
+}
