87f569dc7a4991a715750290ea8f6540ca707c20
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2022-02-14 19:09:37 +0100

    tests: add custom theory data class

---
+++ tests/FileSizeFromBase64.NET.Tests/Base64File.cs
@@ -0,0 +1,13 @@
+﻿namespace FileSizeFromBase64.NET.Tests
+{
+    public class Base64File
+    {
+        public string? Base64String { get; set; }
+
+        public int Size { get; set; }
+
+        public string? Description { get; set; }
+
+        public bool UsePaddingsRules { get; set; }
+    }
+}
---
+++ tests/FileSizeFromBase64.NET.Tests/Base64FileTheoryData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Xunit;
+
+namespace FileSizeFromBase64.NET.Tests
+{
+    public class Base64FileTheoryData : TheoryData<Base64File>
+    {
+        public Base64FileTheoryData()
+        {
+            var json = File.ReadAllBytes("data.txt");
+            var files = JsonSerializer.Deserialize<List<Base64File>>(json);
+            if (files != null) files.ForEach(file => Add(file));
+        }
+    }
+}
