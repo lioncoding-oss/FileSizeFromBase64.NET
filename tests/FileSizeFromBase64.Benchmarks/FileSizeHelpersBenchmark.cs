3657a5b8a9aceba2ad4185a6454405f6dfdd8900
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2026-02-11 08:21:59 +0100

    refactor(benchmarks): upgrade to .NET 10 and enhance benchmark configurations

--- tests/FileSizeFromBase64.Benchmarks/FileSizeHelpersBenchmark.cs
+++ tests/FileSizeFromBase64.Benchmarks/FileSizeHelpersBenchmark.cs
@@ -1,24 +1,48 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.IO;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Reports;
 using FileSizeFromBase64.NET;
 
 namespace FileSizeFromBase64.Benchmarks;
 
-[MediumRunJob]
-[MemoryDiagnoser]
+[Config(typeof(Config))]
 public class FileSizeHelpersBenchmark
 {
-    private readonly string _base64;
-    
-    public FileSizeHelpersBenchmark()
+    private sealed class Config : ManualConfig
     {
-        _base64 = File.ReadAllText("data.txt");
+        public Config()
+        {
+            var baseJob = Job.MediumRun;
+
+            AddJob(baseJob.WithNuGet("FileSizeFromBase64.NET", "1.0.0").WithId("v1.0.0").WithBaseline(true));
+            AddJob(baseJob.WithNuGet("FileSizeFromBase64.NET", "2.0.0").WithId("v2.0.0"));
+
+            AddDiagnoser(MemoryDiagnoser.Default);
+            SummaryStyle = SummaryStyle.Default.WithRatioStyle(RatioStyle.Percentage);
+            Orderer = new DefaultOrderer(SummaryOrderPolicy.FastestToSlowest);
+        }
     }
-    
+
+    private string _rawBase64 = null!;
+    private string _mimeBase64 = null!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _rawBase64 = File.ReadAllText("data.txt");
+        _mimeBase64 = "data:text/plain;base64," + _rawBase64;
+    }
+
     [Benchmark]
-    public double  GetFileSizeFromBase64StringV1() => FileSizeHelpersV1
-        .GetFileSizeFromBase64String(_base64);
-    
+    public double GetFileSizeFromRawBase64String()
+        => FileSizeHelpers.GetFileSizeFromBase64String(_rawBase64);
+
     [Benchmark]
-    public double  GetFileSizeFromBase64StringV2() => FileSizeHelpers
-        .GetFileSizeFromBase64String(_base64);
+    public double GetFileSizeFromMimePrefixedBase64String()
+        => FileSizeHelpers.GetFileSizeFromBase64String(_mimeBase64);
 }
