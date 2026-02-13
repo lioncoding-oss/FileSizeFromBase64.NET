bb7b72961ccb93211d2ff768b5f3f3ab16438402
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2026-02-13 13:53:02 +0100

    perf: avoid memory allocation and mime based string slow issues

--- src/FileSizeFromBase64.NET/FileSizeHelpers.cs
+++ src/FileSizeFromBase64.NET/FileSizeHelpers.cs
@@ -16,49 +16,35 @@ namespace FileSizeFromBase64.NET
         /// <returns>The size of the file represented by the base64 string.</returns>
         public static double GetFileSizeFromBase64String(string base64String, bool applyPaddingsRules = false, UnitsOfMeasurement unitsOfMeasurement = UnitsOfMeasurement.Byte)
         {
-            if (string.IsNullOrEmpty(base64String)) return 0;
+            if (string.IsNullOrEmpty(base64String)) return 0d;
 
-            var base64Length = base64String
-                .AsSpan()
-                [(base64String.IndexOf(',', StringComparison.Ordinal) + 1)..]
-                .Length;
+            ReadOnlySpan<char> base64ReadOnlySpan = base64String.AsSpan();
+
+            // RFC 2397 https://www.rfc-editor.org/rfc/rfc2397.html data:[<mediatype>][;base64],<data>
+            if (base64ReadOnlySpan.StartsWith("data:".AsSpan()))
+            {
+                var commaIndex = base64String.IndexOf(',');
+                if (commaIndex >= 0)
+                    base64ReadOnlySpan = base64ReadOnlySpan[(commaIndex + 1)..];
+            }
+
+            var base64Length = base64ReadOnlySpan.Length;
 
             var fileSizeInByte = Math.Ceiling((double)base64Length / 4) * 3;
 
             if (applyPaddingsRules && base64Length >= 2)
             {
-                ReadOnlySpan<char> paddings = base64String.AsSpan()[^2..];
+                ReadOnlySpan<char> paddings = base64ReadOnlySpan[^2..];
 
                 fileSizeInByte = paddings switch
                 {
-                    _ when paddings.Equals("==", StringComparison.Ordinal) => fileSizeInByte - 2,
-                    _ when paddings[1].Equals('=') => fileSizeInByte - 1,
+                    _ when paddings[0] == '=' && paddings[1] == '=' => fileSizeInByte - 2,
+                    _ when paddings[1] == '=' => fileSizeInByte - 1,
                     _ => fileSizeInByte
                 };
             }
 
             return fileSizeInByte > 0 ? fileSizeInByte / (int)unitsOfMeasurement : 0;
         }
-    }
-
-    /// <summary>
-    /// Unit of measurement.
-    /// </summary>
-    public enum UnitsOfMeasurement
-    {
-        /// <summary>
-        /// B.
-        /// </summary>
-        Byte = 1,
-
-        /// <summary>
-        /// KB.
-        /// </summary>
-        KiloByte = 1_024,
-
-        /// <summary>
-        /// MB.
-        /// </summary>
-        MegaByte = 1_048_576
     }
 }
---
+++ src/FileSizeFromBase64.NET/UnitsOfMeasurement.cs
@@ -0,0 +1,22 @@
+namespace FileSizeFromBase64.NET;
+
+/// <summary>
+/// Unit of measurement.
+/// </summary>
+public enum UnitsOfMeasurement
+{
+    /// <summary>
+    /// B.
+    /// </summary>
+    Byte = 1,
+
+    /// <summary>
+    /// KB.
+    /// </summary>
+    KiloByte = 1_024,
+
+    /// <summary>
+    /// MB.
+    /// </summary>
+    MegaByte = 1_048_576
+}
